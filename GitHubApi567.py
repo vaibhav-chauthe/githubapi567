57174208d132f0f37df6495d666ac66d99ac2bd8
Vaibhav <vaibhavchauthe1209@gmail.com>
2022-11-02 19:01:01 -0400

    added githubapi567.py

--- GitHubApi567.py
+++ GitHubApi567.py
@@ -1,52 +1,47 @@
-import json
-import requests
+from githubClient import GitHubCL as ghcl
+from constants import USERNAME
 
+class fetchUserDetailsServiceImpl():
+    def __init__(self) -> None:
+        pass
 
-def count_user_commits(user):
-    r = requests.get('https://api.github.com/users/%s/repos' % user)
-    repos = json.loads(r.content)
+    def fetchUserRepos(self, username):
+        print("User IN")
+        try:
+            if not username.strip():
+               raise ValueError("Username cannot be null")
+            responseList = ghcl.fetch_user_repo(username)
+            reposList = []
+            for repo in responseList:
+                if(repo['name'].strip()):
+                    reposList.append(repo['name'])
+                else:
+                    raise ValueError("Repo Name cannot be null. Username: %s" % (username))
+            return self.fetchReposCommits(username, reposList)
+        except ValueError as e:
+            print(e)
+            raise ValueError
 
-    for repo in repos:
-        if repo['fork'] is True:
-            # skip it
-            continue
-        n = count_repo_commits(repo['url'] + '/commits')
-        repo['num_commits'] = n
-        yield repo
+    def fetchReposCommits(self, username, reposList):
+        print("REPO IN")
+        username = USERNAME if not username.strip() else username
+        outputList = []
+        try:
+            if(len(reposList)>0):
+                for repos in reposList:
+                    responseList = ghcl.fetch_repo_commits(username,repos)
+                    output = {
+                              "Repo": repos, 
+                              "Number of commits": len(responseList)
+                              }
+                    outputList.append(output)
+            else:
+                raise Exception("No Repos found for username: %s" % (USERNAME))
+        except Exception as e:
+            print(e)
+        [print (output) for output in outputList]
+        return outputList
 
-
-def count_repo_commits(commits_url, _acc=0):
-    r = requests.get(commits_url)
-    commits = json.loads(r.content)
-    n = len(commits)
-    if n == 0:
-        return _acc
-    link = r.headers.get('link')
-    if link is None:
-        return _acc + n
-    next_url = find_next(r.headers['link'])
-    if next_url is None:
-        return _acc + n
-    # try to be tail recursive, even when it doesn't matter in CPython
-    return count_repo_commits(next_url, _acc + n)
-
-
-# given a link header from github, find the link for the next url which they use for pagination
-def find_next(link):
-    for l in link.split(','):
-        a, b = l.split(';')
-        if b.strip() == 'rel="next"':
-            return a.strip()[1:-1]
-
-
-if __name__ == '__main__':
-    import sys
-    try:
-        user = input("github userid:")
-    except IndexError:
-        print ("Usage: %s" % sys.argv[0])
-        sys.exit(1)
-    total = 0
-    for repo in count_user_commits(user):
-        print ("Repo `%(name)s` has %(num_commits)d commits " % repo)
-        
+if __name__ == "__main__":
+    #fetchUserDetailsServiceImpl().fetchUserRepos(" ")
+    fetchUserDetailsServiceImpl().fetchUserRepos("vaibhav-chauthe")
